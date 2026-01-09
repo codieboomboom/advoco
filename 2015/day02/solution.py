06ab62a7b13ff5bdcf28e3fb6275e458e0757687
Anh Tu Do <anhtu.do1998@gmail.com>
2026-01-09 16:44:29 +0800

    [2015][day1][part1] Added solution with simple/none imported

---
+++ 2015/day02/solution.py
@@ -0,0 +1,35 @@
+from typing import List
+
+def read_inputs_as_list(filename: str) -> List[str]:
+    with open(filename, "r", encoding='utf-8') as f:
+        lines = f.readlines()
+    return lines
+
+def convert_line_str_into_dim_list(line: str) -> List[int]:
+    dimensions = line.strip().split(sep="x")
+    dimensions = [int(dim) for dim in dimensions]
+    return dimensions
+
+def part1_simple(dimensions_list: List[int]) -> int:
+    boxes_surface_areas = [] # per each box
+    boxes_total_areas = 0
+    surface_D = 2
+    for dimensions in dimensions_list:
+        print(f"Dimensions of this box: {dimensions}")
+        this_box_areas = []
+        start_idx = 0
+        while start_idx <= len(dimensions) - surface_D:
+            for end_index in range(start_idx+1, len(dimensions)):
+                this_box_areas.append(dimensions[start_idx] * dimensions[end_index])
+                boxes_total_areas += 2 * dimensions[start_idx] * dimensions[end_index]
+            start_idx += 1
+        print(f"This box surfaces areas: {this_box_areas}")
+        boxes_surface_areas.append(this_box_areas)
+    lowest_surface_areas = [min(surface_areas_of_this_box) for surface_areas_of_this_box in boxes_surface_areas]
+    return sum(lowest_surface_areas) + boxes_total_areas
+
+if __name__=="__main__":
+    list_of_input_lines = read_inputs_as_list("input.txt")
+    list_of_dimensions = [convert_line_str_into_dim_list(line) for line in list_of_input_lines]
+    print(list_of_dimensions)
+    print(part1_simple(list_of_dimensions))
